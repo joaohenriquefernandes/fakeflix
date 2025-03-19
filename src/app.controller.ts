c451cdffcedcae28bc724049899225dec0024533
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-03-19 11:20:56 -0300

    feat: create upload video route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { randomUUID } from 'node:crypto';
+import { extname } from 'node:path';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +22,47 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('video')
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 },
+      ],
+      {
+        dest: './uploads',
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (_req, file, cb) => {
+            return cb(
+              null,
+              `${Date.now()}-${randomUUID()}${extname(file.originalname)}`,
+            );
+          },
+        }),
+        fileFilter: (_req, file, cb) => {
+          if (file.mimetype !== 'video/mp4' && file.mimetype !== 'image/jpeg') {
+            return cb(
+              new BadRequestException(
+                'Invalid file type. Only video/mp4 and image/jpeg are suppoted.',
+              ),
+              false,
+            );
+          }
+          return cb(null, true);
+        },
+      },
+    ),
+  )
+  async uploadVideo(
+    @Req() _req: Request,
+    @UploadedFile()
+    files: { video?: Express.Multer.File[]; thumbnail?: Express.Multer.File[] },
+  ): Promise<string> {
+    console.log(files);
+    return 'video uploaded';
   }
 }
